9d3a314eb306e559d710bdb3a8972961250281dc
Veerle Deschepper <veerle@daktadeo.be>
2020-11-07 20:12:25 +0100

    build: breadcrumb

---
+++ components/Breadcrumbs.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="flex" aria-label="Breadcrumb">
+    <ol
+      class="max-w-screen-xl w-full mx-auto px-4 flex space-x-4 sm:px-6 lg:px-8 my-4"
+    >
+      <li class="flex">
+        <div class="flex items-center">
+          <a href="/" class="text-gray-400 hover:text-gray-500">
+            <svg
+              class="flex-shrink-0 h-5 w-5 transition duration-150 ease-in-out"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+              />
+            </svg>
+          </a>
+          <span class="sr-only">Home</span>
+        </div>
+      </li>
+      <li v-for="link in crumbs" :key="link.identifier" class="flex">
+        <div class="flex items-center space-x-4">
+          <svg
+            class="flex-shrink-0 h-5 w-5 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <nuxt-link
+            v-if="link.url"
+            class="text-sm leading-5 font-medium text-gray-600 hover:text-gray-700 transition duration-150 ease-in-out"
+            :to="link.url"
+            >{{ link.name }}
+          </nuxt-link>
+          <span v-else class="text-sm leading-5 font-medium text-gray-400"
+            >{{ link.name }}
+          </span>
+        </div>
+      </li>
+    </ol>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Breadcrumbs',
+  props: {
+    crumbs: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
--- pages/3d/prints/_slug.vue
+++ pages/3d/prints/_slug.vue
@@ -1,112 +1,117 @@
 <template>
-  <div v-if="post" class="w-full max-w-screen-xl mx-auto">
-    <div class="px-4 sm:px-6 lg:px-8 py5 md:py-4">
-      <h1
-        class="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl mb-2"
-      >
-        {{ post.title }}
-      </h1>
-
-      <section v-if="post.print_categories" class="my-4 mb-12">
-        <h2
-          class="text-2xl leading-4 tracking-tight sm:text-3xl mb-4 font-extrabold"
+  <div v-if="post">
+    <Breadcrumbs :crumbs="crumbs"></Breadcrumbs>
+    <div class="w-full max-w-screen-xl mx-auto">
+      <div class="px-4 sm:px-6 lg:px-8 py5 md:py-4">
+        <h1
+          class="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl mb-2"
         >
-          Gerelateerde producten
-        </h2>
-        <ul class="flex space-x-2 flex-wrap">
-          <li v-for="cat in post.print_categories" :key="cat" class="mt-1.5">
-            <span
-              class="whitespace-no-wrap inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium leading-5 border text-gray-800"
-            >
-              {{ cat }}
-            </span>
-          </li>
-        </ul>
-      </section>
+          {{ post.title }}
+        </h1>
 
-      <section v-if="post.body" class="my-4 mb-12">
-        <h2
-          class="text-2xl leading-4 tracking-tight sm:text-3xl mb-6 font-extrabold"
-        >
-          Productinformatie
-        </h2>
-        <h3
-          class="text-1xl leading-4 tracking-tight sm:text-2xl mb-2 font-extrabold"
-        >
-          Omschrijving
-        </h3>
-        <div class="markdown">
-          <nuxt-content :document="post" />
-        </div>
-      </section>
-
-      <section class="my-4 mb-12">
-        <h2
-          class="text-2xl leading-4 tracking-tight sm:text-3xl mb-6 font-extrabold"
-        >
-          Specificaties
-        </h2>
-
-        <div v-if="post.productProperties" class="my-4">
-          <h3 class="text-lg leading-6 font-medium text-gray-900">Product</h3>
-          <div>
-            <dl>
-              <div
-                v-for="(item, key) in post.productProperties"
-                :key="key"
-                class="bg-gray-50 px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+        <section v-if="post.print_categories" class="my-4 mb-12">
+          <h2
+            class="text-2xl leading-4 tracking-tight sm:text-3xl mb-4 font-extrabold"
+          >
+            Gerelateerde producten
+          </h2>
+          <ul class="flex space-x-2 flex-wrap">
+            <li v-for="cat in post.print_categories" :key="cat" class="mt-1.5">
+              <span
+                class="whitespace-no-wrap inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium leading-5 border text-gray-800"
               >
-                <dt class="text-sm leading-5 font-medium text-gray-500">
-                  {{ item }}
-                </dt>
-                <dd
-                  class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2"
-                >
-                  {{ key }}
-                </dd>
-              </div>
-            </dl>
-          </div>
-        </div>
+                {{ cat }}
+              </span>
+            </li>
+          </ul>
+        </section>
 
-        <div v-if="post.printProperties" class="my-4">
-          <h3 class="text-lg leading-6 font-medium text-gray-900">Product</h3>
-          <div>
-            <dl>
-              <div
-                v-for="(item, key) in post.printProperties"
-                :key="key"
-                class="bg-gray-50 px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
-              >
-                <dt class="text-sm leading-5 font-medium text-gray-500">
-                  {{ key }}
-                </dt>
-                <dd
-                  class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2"
-                >
-                  {{ item }}
-                </dd>
-              </div>
-            </dl>
+        <section v-if="post.body" class="my-4 mb-12">
+          <h2
+            class="text-2xl leading-4 tracking-tight sm:text-3xl mb-6 font-extrabold"
+          >
+            Productinformatie
+          </h2>
+          <h3
+            class="text-1xl leading-4 tracking-tight sm:text-2xl mb-2 font-extrabold"
+          >
+            Omschrijving
+          </h3>
+          <div class="markdown">
+            <nuxt-content :document="post" />
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section>
-        <div class="leading-6 text-gray-500 text-sm">
-          Laatste aanpassing:
-          <time :datetime="post.updatedAt">
-            {{ post.updatedAt | formatDateTimeLong }}
-          </time>
-        </div>
-      </section>
+        <section class="my-4 mb-12">
+          <h2
+            class="text-2xl leading-4 tracking-tight sm:text-3xl mb-6 font-extrabold"
+          >
+            Specificaties
+          </h2>
+
+          <div v-if="post.productProperties" class="my-4">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">Product</h3>
+            <div>
+              <dl>
+                <div
+                  v-for="(item, key) in post.productProperties"
+                  :key="key"
+                  class="bg-gray-50 px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+                >
+                  <dt class="text-sm leading-5 font-medium text-gray-500">
+                    {{ item }}
+                  </dt>
+                  <dd
+                    class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2"
+                  >
+                    {{ key }}
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+
+          <div v-if="post.printProperties" class="my-4">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">Product</h3>
+            <div>
+              <dl>
+                <div
+                  v-for="(item, key) in post.printProperties"
+                  :key="key"
+                  class="bg-gray-50 px-4 py-2 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
+                >
+                  <dt class="text-sm leading-5 font-medium text-gray-500">
+                    {{ key }}
+                  </dt>
+                  <dd
+                    class="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2"
+                  >
+                    {{ item }}
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div class="leading-6 text-gray-500 text-sm">
+            Laatste aanpassing:
+            <time :datetime="post.updatedAt">
+              {{ post.updatedAt | formatDateTimeLong }}
+            </time>
+          </div>
+        </section>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import Breadcrumbs from '~/components/Breadcrumbs'
 export default {
   name: 'DddPrint',
+  components: { Breadcrumbs },
   async asyncData(context) {
     const { $content, params, app } = context
     const slug = params.slug
@@ -115,6 +120,26 @@ export default {
     return {
       post,
     }
+  },
+  computed: {
+    crumbs() {
+      return [
+        {
+          identifier: '3d',
+          name: '3D',
+          url: `/3d`,
+        },
+        {
+          identifier: 'prints',
+          name: 'prints',
+        },
+        {
+          identifier: this.post.slug,
+          name: this.post.title,
+          url: `/3d/prints/${this.post.slug}`,
+        },
+      ]
+    },
   },
 }
 </script>
