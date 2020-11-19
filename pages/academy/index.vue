4fc4a46bd7ee301720925f8ded2683a8ac1cd5fb
Veerle <veerle@daktadeo.be>
2020-11-19 10:31:29 +0100

    build: min academy page

---
+++ pages/academy/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-if="page">
+    <Hero
+      :title="page.hero.title_1"
+      :sub-title="page.hero.title_2"
+      :tag-line="page.hero.tagline"
+    ></Hero>
+
+    <Features :items="features"></Features>
+    <under-construction></under-construction>
+    <Sections :items="sections"></Sections>
+  </div>
+</template>
+
+<script>
+import Hero from '@/components/Hero'
+import Features from '@/components/Features'
+import Sections from '@/components/Sections'
+import UnderConstruction from '~/components/UnderConstruction'
+
+export default {
+  components: { UnderConstruction, Sections, Features, Hero },
+  async asyncData(context) {
+    const { $content, app } = context
+    const defaultLocale = app.i18n.locale
+    const page = await $content(`${defaultLocale}/academy/index`).fetch()
+
+    return {
+      page,
+    }
+  },
+  computed: {
+    features() {
+      return _.sortBy(this.page.features, 'weight')
+    },
+    sections() {
+      return _.sortBy(this.page.sections, 'weight')
+    },
+  },
+}
+</script>
+<style scoped>
+.hero {
+  background-image: linear-gradient(
+      to right bottom,
+      rgba(246, 246, 246, 0.25),
+      rgb(48, 72, 75)
+    ),
+    url('~assets/img/hero.jpg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
