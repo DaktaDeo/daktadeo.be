9d9dcc2b22f27b398be82a0566d36b5893445b2b
Veerle <veerle@daktadeo.be>
2020-12-11 12:03:22 +0100

    fix: hero banner

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="bg-primary-color-light flex flex-col h-screen justify-between">
+  <div
+    class="bg-primary-color-light flex flex-col min-h-screen justify-between h-full"
+  >
     <Header :nav="nav.header" :nav-mobile="nav.mobile"></Header>
-    <Nuxt />
+    <div class="antialiased text-gray-900 min-h-full flex-1 flex-col">
+      <Nuxt />
+    </div>
     <Footer :socials="socials.follow" :nav="nav.footer"></Footer>
   </div>
 </template>
 
+<style>
+body {
+  @apply font-sans text-black leading-tight antialiased bg-gray-100;
+}
+</style>
 <script>
 // class="mb-auto"
 import Footer from '@/components/Footer'
